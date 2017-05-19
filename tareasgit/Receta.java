387ca917e1dfc67e124f8371f235105e0ccbe869
Pedro Juan <pedrotorrano@hotmail.com>
2017-05-19 16:45:23 +0200

    Añadidos constructores de Receta
    Signed-off-by: Pedro Juan <pedrotorrano@hotmail.com>

--- tareasgit/Receta.java
+++ tareasgit/Receta.java
@@ -5,4 +5,15 @@ public class Receta {
 	private String ingredientes;
 	private String preparacion;
 	
+	public Receta(){
+		
+	}
+	
+	public Receta(String nombre, String ingredientes, String preparacion) {
+		this.nombre = nombre;
+		this.ingredientes = ingredientes;
+		this.preparacion = preparacion;
+	}
+	
+	
 }
