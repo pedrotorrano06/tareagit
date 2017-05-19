5f2e25f4875f3dc4a64a9c97cee5bf6104f257a8
Pedro Juan <pedrotorrano@hotmail.com>
2017-05-19 16:48:37 +0200

    Añadido constructor, Getters y Setters
    Signed-off-by: Pedro Juan <pedrotorrano@hotmail.com>

--- tareasgit/Ingrediente.java
+++ tareasgit/Ingrediente.java
@@ -3,5 +3,32 @@ package tareasgit;
 public class Ingrediente {
 	private String cantidad;
 	private String nombre;
+	
+	public Ingrediente(){
+		
+	}
+	
+	public Ingrediente(String cantidad, String nombre) {
+		this.cantidad = cantidad;
+		this.nombre = nombre;
+	}
+
+	public String getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(String cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
 
 }
